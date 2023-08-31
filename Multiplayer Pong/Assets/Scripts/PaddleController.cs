8b1d1883a6ba562f7e390d858934810f9a75a095
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-08-31 13:22:39 +0100

    Added the initial unity project and did some of the initial development
    Just added the unity project. Also added some of the simple game elements

---
+++ Multiplayer Pong/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float paddleSpeed = 5.0f;
+    public float boundaryY = 2.5f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the left paddle
+        if (Input.GetKey(KeyCode.W))
+        {
+            MovePaddle(Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            MovePaddle(Vector3.down);
+        }
+
+        // Move the right paddle
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MovePaddle(Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MovePaddle(Vector3.down);
+        }
+    }
+   
+    void MovePaddle(Vector3 direction)
+    {
+        // Calculate new position
+        Vector3 newPosition = transform.position + direction * paddleSpeed * Time.deltaTime;
+
+        // Clamp the paddle's position within the play area
+        newPosition.y = Mathf.Clamp(newPosition.y, -boundaryY, boundaryY);
+
+        // Apply the new position
+        transform.position = newPosition;
+    }
+}
