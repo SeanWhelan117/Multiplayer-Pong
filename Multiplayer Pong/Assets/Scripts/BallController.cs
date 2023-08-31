41230644d10ad5289314d618f11c8ff9f8e6c22c
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-08-31 16:33:14 +0100

    added ball resetting and some score.
    Adding UI to represent the score next

--- Multiplayer Pong/Assets/Scripts/BallController.cs
+++ Multiplayer Pong/Assets/Scripts/BallController.cs
@@ -10,20 +10,28 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        randStartY = Random.Range(-5, 5);
-        transform.position = new Vector3(0.0f, randStartY, 0.0f);
 
-
-        Rigidbody2D ballRB = GetComponent<Rigidbody2D>();
-        Vector3 initialDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
-        ballRB.AddForce(initialDirection * velocity, ForceMode2D.Impulse);
+        InitialiseBall();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < -13 || transform.position.x > 13)
+        {
+            InitialiseBall();
+        }
     }
 
-   
+    private void InitialiseBall()
+    {
+        randStartY = Random.Range(-5, 5);
+        transform.position = new Vector3(0.0f, randStartY, 0.0f);
+        Debug.Log("Set");
+
+        Rigidbody2D ballRB = GetComponent<Rigidbody2D>();
+        Vector3 initialDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
+        ballRB.velocity = Vector3.zero;
+        ballRB.AddForce(initialDirection * velocity, ForceMode2D.Impulse);
+    }
 }
--- Multiplayer Pong/Assets/Scripts/GameController.cs
+++ Multiplayer Pong/Assets/Scripts/GameController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject ball;
+
+    public int scorePlayer1;
+    public int scorePlayer2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,15 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject ballClone = GameObject.FindGameObjectWithTag("Ball");
+
+        if (ballClone.transform.position.x < -13)
+        {
+            scorePlayer2 += 1;
+        }
+        else if (ballClone.transform.position.x > 13)
+        {
+            scorePlayer1 += 1;
+        }
     }
 }
