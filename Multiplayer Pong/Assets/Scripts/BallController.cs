40ba81fe0e31614418ab5e36230dcbe7f47ef8d9
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-09-01 10:40:34 +0100

    added simple menu and scene transition

--- Multiplayer Pong/Assets/Scripts/BallController.cs
+++ Multiplayer Pong/Assets/Scripts/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
 {
     public float velocity = 5.0f;
     public float randStartY;
+    public Vector3 initialDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +31,8 @@ public class BallController : MonoBehaviour
         Debug.Log("Set");
 
         Rigidbody2D ballRB = GetComponent<Rigidbody2D>();
-        Vector3 initialDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
+
+        initialDirection = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0f).normalized;
         ballRB.velocity = Vector3.zero;
         ballRB.AddForce(initialDirection * velocity, ForceMode2D.Impulse);
     }
