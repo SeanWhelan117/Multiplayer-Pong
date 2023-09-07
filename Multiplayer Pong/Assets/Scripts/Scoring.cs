4c68076ea19254e1281b9893d59da6759df26e6d
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-09-07 11:00:12 +0100

    WIP build added

--- Multiplayer Pong/Assets/Scripts/Scoring.cs
+++ Multiplayer Pong/Assets/Scripts/Scoring.cs
@@ -8,18 +8,30 @@ public class Scoring : MonoBehaviour
 {
     [SerializeField] private LeaderboardRef _leaderboardRef = null;
     [SerializeField] private double _score = 100;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log($"Start()");
-
         LeaderboardServiceSetScore(_leaderboardRef.Id, _score);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space presesd");
+            double score = _score + 15; 
+            LeaderboardServiceSetScore(_leaderboardRef.Id, score);
+        }
+
+        GameObject ball = GameObject.Find("Ball");
+
+        if(ball.transform.position.x > 1000 || ball.transform.position.x < -1000)
+        {
+            LeaderboardServiceSetScore(_leaderboardRef.Id, 50);
+        }
     }
 
     //  Methods  --------------------------------------
